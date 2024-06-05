570b7a4a4b3e5f34fc4c9992094f5babea3438ec
Mialenna <annakupriyanova04@mail.ru>
2024-06-05 22:06:54 +0700

    cocomo

--- cocomo/js/main.js
+++ cocomo/js/main.js
@@ -1,3 +1,17 @@
+function openInsert(evt, insertName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(insertName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 let arr = [  [3.2, 1.05],  
 		     [3.0, 1.12],  
 		     [2.8, 1.20]   
